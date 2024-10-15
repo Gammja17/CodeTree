acead1cee66e49feac2f9c7048ffb27ed6996edf
Gammja17 <127707535+Gammja17@users.noreply.github.com>
2024-10-15 19:01:25 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241015/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d,e;
+
+    cin >>a>>b>>c>>d>>e;
+
+    if(a>b) cout<<"1\n";
+    else if(a<=b) cout<<"0\n";
+
+    if(a>c) cout<<"1\n";
+    else if(a<=c) cout<<"0\n";
+
+    if(a>d) cout<<"1\n";
+    else if(a<=d) cout<<"0\n";
+
+    if(a>e) cout<<"1\n";
+    else if(a<=e) cout<<"0\n";
+
+
+    return 0;
+}
