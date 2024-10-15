96126a3098e136177e28e8c48961fc4a5118a260
Gammja17 <127707535+Gammja17@users.noreply.github.com>
2024-10-15 20:09:25 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241015/두 사람/two-person.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    char ga,gb;
+    cin >> a>>ga>>b>>gb;
+
+    if((a>=19&&ga=='M')||(b>=19&&gb=='W')) cout<<"1";
+    else cout<<"0";
+    return 0;
+}
