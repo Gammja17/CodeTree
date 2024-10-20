97333ff516e2888fb5113e6da6b333cc8f0b856e
Gammja17 <127707535+Gammja17@users.noreply.github.com>
2024-10-20 20:33:34 +0900

    Title: 청소당번 정하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241020/청소당번 정하기/cleaning-numbering.cpp
+++ 241020/청소당번 정하기/cleaning-numbering.cpp
@@ -7,7 +7,7 @@ int main() {
     cin>>n;
     int a=0,b=0,c=0;
 
-    for(int i=0; i<n; i++){
+    for(int i=1; i<=n; i++){
         if((i%2==0||i%3==0)&&i%12==0) c+=1;
         else if(i%2==0&&i%3==0) b+=1;
         else if(i%2==0) a+=1;
