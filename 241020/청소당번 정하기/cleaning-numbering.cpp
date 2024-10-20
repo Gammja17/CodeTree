de00dfcd7abf97272ed0c1aa801912f9ab097101
Gammja17 <127707535+Gammja17@users.noreply.github.com>
2024-10-20 20:33:11 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241020/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int a=0,b=0,c=0;
+
+    for(int i=0; i<n; i++){
+        if((i%2==0||i%3==0)&&i%12==0) c+=1;
+        else if(i%2==0&&i%3==0) b+=1;
+        else if(i%2==0) a+=1;
+        else if(i%3==0) b+=1;
+        else if(i%12==0) c+=1;
+        else if(i<12) c=0;
+        else if(i<3) c=0, b=0;
+        else if(i<2) a=0,b=0,c=0;
+    }
+    cout<<a<<" "<<b<<" "<<c;
+    return 0;
+}
