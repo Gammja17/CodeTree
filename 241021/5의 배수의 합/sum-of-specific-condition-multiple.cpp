f1052013d515ace0de9ec118e8611658fdfeefc7
Gammja17 <127707535+Gammja17@users.noreply.github.com>
2024-10-21 03:25:28 +0900

    Title: 5의 배수의 합, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241021/5의 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 241021/5의 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -5,18 +5,21 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b,sum;
     cin>>a>>b;
+    
     if(a>=b){
-        for(int i=a;i<=b; i++){
+        for(int i=a;i<=b; i++)
+        {
         if(i%5==0) sum+=i;
         }
         cout<<sum;   
     }
 
-      else if(b>a){
-        for(int i=b;i<=a; i++){
-        if(i%5==0) sum+=i;
-    }
-    cout<<sum;
+    else if(b>a){
+        for(int k=b;k<=a; k++)
+        {
+        if(k%5==0) sum+=k;
+        }
+        cout<<sum;
     }
     return 0;
 }
