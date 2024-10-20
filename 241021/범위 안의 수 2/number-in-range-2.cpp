8a5755a2a51850b3dd6ec90e0df5b78857a2bbc7
Gammja17 <127707535+Gammja17@users.noreply.github.com>
2024-10-21 04:29:50 +0900

    Title: 범위 안의 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241021/범위 안의 수 2/number-in-range-2.cpp
+++ 241021/범위 안의 수 2/number-in-range-2.cpp
@@ -5,13 +5,18 @@ int main() {
     // 여기에 코드를 작성해주세요.
 
     int n, sum=0;
+    double av=0,count=0;
     for(int i=0;i<10;i++){
             cin>>n;
             if(n>=0&&n<=200){
                 sum+=n;
+                count+=1;
             }
     }
-    cout<<sum;
+    cout<<fixed;
+    cout.precision(1);
+    av=sum/count;
+    cout<<sum<<" "<<av;
 
     return 0;
 }
