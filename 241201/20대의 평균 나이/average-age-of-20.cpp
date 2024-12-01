24ddb4a9b94ed9a530eed0e31bca8159f20a75ed
Gammja17 <127707535+Gammja17@users.noreply.github.com>
2024-12-01 18:52:16 +0900

    Title: 20대의 평균 나이, Time: 1071ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 241201/20대의 평균 나이/average-age-of-20.cpp
+++ 241201/20대의 평균 나이/average-age-of-20.cpp
@@ -5,22 +5,20 @@ int main() {
     // 여기에 코드를 작성해주세요.
 
 
-    int  count=0, age;
-    double sum=0,av;
+    int age;
+    double sum=0, count=0 ,av;
 
     cout<<fixed;
     cout.precision(2);
 
-    while(true){
+    while(1){
         cin>>age;
-
         if(age>=30){
-            cout<<av;
+            cout<<sum/count;
             break;
         }
         count+=1;
         sum+=age;
-        av=sum/count;
     }
     return 0;
 }
